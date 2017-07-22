c61a7d97579f4ccf7dcaf11b1ba788bab776eaec
Dan Tanham <dan.tanham@gmail.com>
2017-07-22 11:44:43 +0100

    Added words from query parameter to image

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import qrcode
 import StringIO
+from PIL import ImageFont
+from PIL import ImageDraw
 
 import flask
 app = flask.Flask(__name__)
@@ -16,5 +18,13 @@ def generate_qr():
 	url = flask.request.args.get('url')
 	words = flask.request.args.get('words').split(',')
 	img = qrcode.make(url)
+
+	draw = ImageDraw.Draw(img)
+	font = ImageFont.truetype("OpenDyslexic-Regular.otf", 24)
+
+	xoffset = 35
+	yoffset = 3
+	draw.text((xoffset, yoffset)," ".join(words),(0),font=font)
+	del draw
 
 	return serve_pil_image(img)
